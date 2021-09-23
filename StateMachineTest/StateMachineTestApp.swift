1bd9ac5df2d4d40634fd4dd6eb692840aa78e36c
syntakks <syntakks@gmail.com>
2021-09-23 11:01:04 -0500

    Initial Commit

---
+++ StateMachineTest/StateMachineTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  StateMachineTestApp.swift
+//  StateMachineTest
+//
+//  Created by Stephen Wall on 9/23/21.
+//
+
+import SwiftUI
+
+@main
+struct StateMachineTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
